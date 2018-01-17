b53cb7eb3170e6a273ba8126c5db7f8326bea958
jarbasai <jarbasai@mailfence.com>
2018-01-17 02:28:47 +0000

    stt endpoint

--- backend/stt.py
+++ backend/stt.py
@@ -1,7 +1,7 @@
 from backend import app, API_VERSION
 from backend.decorators import noindex, donation, requires_auth
-from flask import redirect, url_for, request, Response
-
+from flask import request
+from tempfile import TemporaryFile
 from speech_recognition import Recognizer, AudioFile
 
 recognizer = Recognizer()
@@ -13,11 +13,11 @@ recognizer = Recognizer()
 @requires_auth
 def stt():
     flac_audio = request.data
-    lang = request.args["lang"]
+    lang = request.args["lang"] or "en-us"
 
-    with open("stt.flac", "r+b") as f:
-        f.write(flac_audio)
-    with AudioFile("stt.flac") as source:
+    fp = TemporaryFile()
+    fp.write(flac_audio)
+    with AudioFile(fp) as source:
         audio = recognizer.record(source)  # read the entire audio file
 
     utterance = recognizer.recognize_google(audio, language=lang)
