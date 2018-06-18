f04a5184ee436f80f4798a64eed19c8fd1b64d65
jarbasal <jarbasai@mailfence.com>
2018-06-18 16:09:23 +0100

    stt endpoint

--- backend/stt.py
+++ backend/stt.py
@@ -1,32 +1,32 @@
-from backend import app, API_VERSION, USE_DEEPSPEECH
-from backend.decorators import noindex, donation, requires_auth
+from tempfile import NamedTemporaryFile
+import json
 from flask import request
-from tempfile import TemporaryFile
-
 from speech_recognition import Recognizer, AudioFile
+
+from backend import app, API_VERSION, USE_DEEPSPEECH
+from backend.decorators import noindex, requires_auth
 
 recognizer = Recognizer()
 
 if USE_DEEPSPEECH:
     from extra.deepspeech_stt import DeepSpeechSTT
+
     engine = DeepSpeechSTT()
     recognize = engine.recognize
 else:
     recognize = recognizer.recognize_google
 
 
-@app.route("/"+API_VERSION+"/stt", methods=['POST'])
+@app.route("/" + API_VERSION + "/stt", methods=['POST'])
 @noindex
-@donation
 @requires_auth
 def stt():
     flac_audio = request.data
-    lang = request.args["lang"] or "en-us"
-
-    with TemporaryFile() as fp:
+    lang = str(request.args.get("lang", "en-us"))
+    with NamedTemporaryFile() as fp:
         fp.write(flac_audio)
-        with AudioFile(fp) as source:
+        with AudioFile(fp.name) as source:
             audio = recognizer.record(source)  # read the entire audio file
 
         utterance = recognize(audio, language=lang)
-    return utterance
+    return json.dumps([utterance])
